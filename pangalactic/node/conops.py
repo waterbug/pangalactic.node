1057f019702c482acb5024b78d333855e165e73a
Steve Waterbury <waterbug@pangalactic.us>
2019-07-05 01:16:41 -0400

    Clean up imports; minor code edits for clarity.

--- pangalactic/node/conops.py
+++ pangalactic/node/conops.py
@@ -1,41 +1,32 @@
 #!/usr/bin/env python
-from __future__  import print_function
-from __future__  import unicode_literals
-from __future__ import division
-from future import standard_library
-standard_library.install_aliases()
-# NOTE: fixed div's so old_div is not needed.
-# from past.utils import old_div
 import os
-import copy
-from collections import namedtuple
-from urllib.parse    import urlparse
+from collections  import namedtuple
+from urllib.parse import urlparse
 
 from louie import dispatcher
 
-from PyQt5.QtCore import Qt, QRectF,QPointF, QSizeF, QObject, pyqtSignal, qrand, QLineF, QPoint, QMimeData
-from PyQt5.QtWidgets import (QAction, QApplication, QComboBox, QHBoxLayout,
-                             QLayout, QMainWindow, QSizePolicy, QVBoxLayout,
-                             QWidget,QAction, QApplication, QButtonGroup, QComboBox,
-        QFontComboBox, QGraphicsItem, QGraphicsLineItem, QGraphicsPolygonItem,
-        QGraphicsScene, QGraphicsTextItem, QGraphicsView, QGridLayout,
-        QHBoxLayout, QLabel, QMainWindow, QMenu, QMessageBox, QSizePolicy,
-        QToolBox, QToolButton, QWidget, QPushButton, QAbstractItemView, QGraphicsPathItem)
-from PyQt5.QtGui import (QIcon, QTransform, QBrush, QColor, QDrag, QImage, QPainter, QPen, QPixmap, QCursor, QPainterPath,
-                        QPolygon, QPolygonF, QFont)
+from PyQt5.QtCore import Qt, QRectF, QPointF, QPoint, QMimeData
+from PyQt5.QtWidgets import (
+        QAction, QApplication, QButtonGroup, QComboBox, QGraphicsItem,
+        QGraphicsPathItem, QGraphicsPolygonItem, QGraphicsScene, QGraphicsView,
+        QGridLayout, QHBoxLayout, QMainWindow, QMenu, QPushButton, QSizePolicy,
+        QToolBox, QWidget)
+from PyQt5.QtGui import (
+        QBrush, QCursor, QDrag, QIcon, QPainter, QPainterPath, QPen, QPixmap,
+        QPolygonF, QTransform)
 
 # pangalactic
 from pangalactic.core             import diagramz, state
-from pangalactic.core.parametrics import componentz
 from pangalactic.core.uberorb     import orb
-from pangalactic.core.utils.meta  import (asciify, get_block_model_id,
-                                             get_block_model_name,
-                                             get_block_model_file_name)
-from pangalactic.node.dialogs     import Viewer3DDialog
+from pangalactic.core.utils.meta  import asciify, get_block_model_file_name
 from pangalactic.node.diagrams    import DocForm
-from pangalactic.node.pgxnobject  import PgxnObject
-from pangalactic.node.utils       import clone, extract_mime_data
-from pangalactic.node.widgets     import NameLabel, PlaceHolder, ValueLabel
+# TODO: uncomment this later when we need PgxnObject
+# from pangalactic.node.pgxnobject  import PgxnObject
+from pangalactic.node.utils       import clone
+# TODO: uncomment this later when we need extract_mime_data
+# from pangalactic.node.utils       import extract_mime_data
+# TODO: uncomment these later as needed for labels etc.
+# from pangalactic.node.widgets     import NameLabel, PlaceHolder, ValueLabel
 
 supported_model_types = {
     # CAD models get "eyes" icon, not a lable button
@@ -123,25 +114,25 @@ class EventBlock(QGraphicsPolygonItem):
         self.shape = shape
         self.setBrush(Qt.white)
         self.create_actions()
-        path = QPainterPath()
+        painter_path = QPainterPath()
         self.activity = activity or clone("Activity")
 
-    #---draw blocks depending on the 'shape' string passed in
+        # draw blocks depending on the 'shape' string passed in
         if self.shape == "Box":
-            self.myPolygon = QPolygonF([
+            polygon = QPolygonF([
                     QPointF(-50, 50), QPointF(50, 50),
                     QPointF(50, -50), QPointF(-50, -50)
             ])
         if self.shape == "Triangle":
-             self.myPolygon = QPolygonF([
-                     QPointF(0, 0), QPointF(-50, 80),
-                     QPointF(50, 80)
-             ])
+            polygon = QPolygonF([
+                    QPointF(0, 0), QPointF(-50, 80),
+                    QPointF(50, 80)
+            ])
         if self.shape == "Circle":
-            path.addEllipse(-50, -50, 100, 100)
-            self.myPolygon = path.toFillPolygon()
+            painter_path.addEllipse(-50, -50, 100, 100)
+            polygon = painter_path.toFillPolygon(QTransform())
             self.setFlag(QGraphicsItem.ItemSendsGeometryChanges)
-        self.setPolygon(self.myPolygon)
+        self.setPolygon(polygon)
 
     def mouseDoubleClickEvent(self, event):
         dispatcher.send("double clicked", obj=self.activity)
@@ -152,7 +143,9 @@ class EventBlock(QGraphicsPolygonItem):
         self.menu.exec(QCursor.pos())
 
     def create_actions(self):
-        self.delete_action = QAction("Delete", self.scene(), statusTip="Delete Item", triggered= self.deleteItem)
+        self.delete_action = QAction("Delete", self.scene(),
+                                     statusTip="Delete Item",
+                                     triggered=self.deleteItem)
 
     def deleteItem(self):
         self.scene().timeline.remove_item(self)
@@ -222,18 +215,19 @@ class Timeline(QGraphicsPathItem):
 
     def make_point_list(self):
         factor = self.length/(self.num_of_item+1)
-        self.list_of_pos = [(n+1)*factor+self.p1.x() for n in range(0, self.num_of_item)]
+        self.list_of_pos = [(n+1)*factor+self.p1.x()
+                            for n in range(0, self.num_of_item)]
 
     def reposition(self):
         self.item_list.sort(key=lambda x: x.scenePos().x())
         for i in range(0, len(self.item_list)):
-            if self.item_list[i].shape == "Triangle":
-                self.item_list[i].setPos(QPoint(self.list_of_pos[i], 150))
-                self.item_list[i].activity.components.reference_designator = u"{}".format(i)
+            item = self.item_list[i]
+            if item.shape == "Triangle":
+                item.setPos(QPoint(self.list_of_pos[i], 150))
+                item.activity.components.reference_designator = str(i)
             else:
-                self.item_list[i].setPos(QPoint(self.list_of_pos[i], 150))
-                self.item_list[i].activity.components.reference_designator = u"{}".format(i)
-
+                item.setPos(QPoint(self.list_of_pos[i], 150))
+                item.activity.components.reference_designator = str(i)
 
     def remove_item(self, item):
         if item in self.item_list:
@@ -260,7 +254,9 @@ class DiagramScene(QGraphicsScene):
     def dropEvent(self, event):
         activity = clone("Activity")
         acu = clone("Acu", assembly=self.parent_activity, component=activity)
-        item = EventBlock(event.mimeData().text(), activity=activity, parent_activity=self.parent_activity)
+        orb.save([acu])
+        item = EventBlock(event.mimeData().text(), activity=activity,
+                          parent_activity=self.parent_activity)
         item.setPos(event.scenePos())
         print(len(self.items()))
         self.timeline.add_item(item)
@@ -335,8 +331,7 @@ class ConOpsModeler(QMainWindow):
             preferred_size (tuple):  size to set -- (width, height)
         """
         super(ConOpsModeler, self).__init__(parent=parent)
-        orb.log.info('* ConOpsModeler initializing with:')
-        orb.log.info('  obj "{}"'.format(getattr(obj, 'oid', 'None')))
+        orb.log.info('* ConOpsModeler initializing')
         self.logo = logo
         self.external = external
         self.idx = idx
@@ -346,7 +341,7 @@ class ConOpsModeler(QMainWindow):
         self.temp_activity = clone("Activity")
         self.setSizePolicy(QSizePolicy.Expanding,
                            QSizePolicy.Expanding)
-#-----------------------------------------------------------#
+        #-----------------------------------------------------------#
         self.createLibrary()
         self.scene = DiagramScene(self, self.temp_activity)
         self.set_new_view(self.scene, parent_activity=self.temp_activity)
@@ -361,9 +356,8 @@ class ConOpsModeler(QMainWindow):
         self.widget.setLayout(layout)
         self.setCentralWidget(self.widget)
         self.widget.setAcceptDrops(True)
-        #------------lisening for signals------------#
+        #------------ listening for signals ------------#
         dispatcher.connect(self.double_clicked_handler, "double clicked")
-
 
     def createLibrary(self):
         '''create the shape library'''
@@ -427,7 +421,6 @@ class ConOpsModeler(QMainWindow):
         self.scene_scale_select.currentIndexChanged[str].connect(
                                                     self.sceneScaleChanged)
         self.toolbar.addWidget(self.scene_scale_select)
-
 
     def create_action(self, text, slot=None, icon=None, tip=None,
                       checkable=False):
@@ -520,6 +513,7 @@ class ConOpsModeler(QMainWindow):
         #     dispatcher.send('diagram go back', index=self.idx)
         # else:
         #     self.back_action.setEnabled(False)
+
     def set_subject_from_diagram_drill_down(self, obj=None):
         """
         Respond to a double-clicked diagram block by setting the corresponding
@@ -549,8 +543,6 @@ class ConOpsModeler(QMainWindow):
         self.set_subject(obj=obj)
 
     def set_subject(self, obj=None):
-
-
         """
         Set an object for the current modeler context.  If the object does not
         have a Block model one is created from its components (or an empty
@@ -612,64 +604,10 @@ class ConOpsModeler(QMainWindow):
         self.view.verticalScrollBar().setValue(0)
         self.view.horizontalScrollBar().setValue(0)
 
-    def display_cad_model(self):
-        try:
-            model, fpath = self.models_by_label.get('CAD')
-            if fpath:
-                orb.log.info('* ConOpsModeler.display_cad_model({})'.format(
-                                                                     fpath))
-                viewer = Viewer3DDialog(self)
-                viewer.show()
-                viewer.view_cad(fpath)
-        except:
-            orb.log.info('  CAD model not found.')
-
-    # def display_block_diagram(self):
-    #     """
-    #     Display a block diagram for the current object.
-    #     """
-    #     orb.log.info('* Modeler:  display_block_diagram()')
-    #     if not getattr(self, 'obj', None):
-    #         orb.log.info('  no object selected.')
-    #         return
-    #     self.set_new_view()
-    #     scene = self.view.scene()
-    #     model = diagramz.get(self.obj.oid)
-    #     objs = []
-    #     if hasattr(self.obj, 'components') and componentz.get(self.obj.oid):
-    #         # self.obj is a Product -- use componentz cache (more efficient
-    #         # than using obj.components ...
-    #         oids = [c[0] for c in componentz[self.obj.oid]]
-    #         objs = orb.get(oids=oids)
-    #     elif hasattr(self.obj, 'systems') and len(self.obj.systems):
-    #         # self.obj is a Project
-    #         objs = [psu.system for psu in self.obj.systems]
-    #     if model and not model.get('dirty'):
-    #         orb.log.info('  - restoring saved block diagram ...')
-    #         scene.restore_diagram(model, objs)
-    #     else:
-    #         if model and model.get('dirty'):
-    #             orb.log.info('  - block diagram found needed redrawing,')
-    #         elif not model:
-    #             orb.log.info('  - no block diagram found in cache or files ')
-    #         orb.log.info('    generating new block diagram ...')
-    #         # orb.log.info('  - generating diagram (cache disabled for testing)')
-    #         scene.create_ibd(objs)
-    #         # create a block Model object if self.obj doesn't have one
-    #         block_model_type = orb.get(BLOCK_OID)
-    #         if self.obj.has_models:
-    #             block_models = [m for m in self.obj.has_models
-    #                 if getattr(m, 'type_of_model', None) == block_model_type]
-    #             if not block_models:
-    #                 model_id = get_block_model_id(self.obj)
-    #                 model_name = get_block_model_name(self.obj)
-    #                 self.model = clone('Model', id=model_id, name=model_name,
-    #                                    type_of_model=block_model_type,
-    #                                    of_thing=self.obj)
-
-    def save_diagram_connector(self, start_item=None, end_item=None):
+    def display_block_diagram(self):
+        # TODO:  this is just a placeholder until we remove the "set_subject()"
+        # stuff ...
         pass
-
 
     def cache_block_model(self):
         """
@@ -690,107 +628,12 @@ class ConOpsModeler(QMainWindow):
         except:
             orb.log.info('  ... could not cache model (C++ obj deleted?)')
 
-    # DEPRECATED:  now using diagramz cache, not block model files
-    # def write_block_model(self, cached_model=None, fpath=None):
-        # """
-        # Write the specified cached block model to a file (or if none is provided, the cached block
-        # model of the current subject).
-
-        # Args:
-            # cached_model (dict): a serialized block model
-
-        # Keyword args:
-            # fpath (str):  path of file to be written.
-        # """
-        # orb.log.info('* Modeler: write_block_model()')
-        # # TODO: find or create a Representation with 'of_object' == model.oid
-        # # and a RepresentationFile that will get the file path as its 'url'
-        # # attribute.
-        # if not cached_model:
-            # if (not self.obj or
-                # not diagramz.get(self.obj.oid)):
-                # orb.log.info('  no cached block model found; returning.')
-                # return
-            # cached_model = diagramz[self.obj.oid]
-        # fname = get_block_model_file_name(self.obj)
-        # if not fpath:
-            # # write to vault if fpath is not given
-            # orb.log.info('  writing to vault: {}'.format(fname))
-            # fpath = os.path.join(orb.vault, fname)
-        # f = open(fpath, 'w')
-        # orb.log.info('  writing to path {} ...'.format(fpath))
-        # f.write(json.dumps(cached_model,
-                           # separators=(',', ':'),
-                           # indent=4, sort_keys=True))
-        # f.close()
-
-    # DEPRECATED:  now using diagramz cache, not block model files
-    # def read_block_model(self, fpath):
-        # """
-        # Read a serialized block model (dict) from the specified file path.
-
-        # Args:
-            # fpath (str): path to a serialized block model file
-        # """
-        # orb.log.info('* Modeler: read_block_model({})'.format(fpath))
-        # if not os.path.exists(fpath):
-            # orb.log.info('  - path does not exist.')
-            # return None
-        # f = open(fpath)
-        # orb.log.info('  reading model from path {} ...'.format(fpath))
-        # m = json.loads(f.read())
-        # f.close()
-        # return m
-
-    # NOTE: 'set_canvas' is not currently used but should be
-    # def set_canvas(self, widget=None, name=None):
-    #     if hasattr(self, 'canvas_widget') and self.canvas_widget is not None:
-    #         self.canvas_box.removeWidget(self.canvas_widget)
-    #         self.canvas_widget.deleteLater()
-    #     if not hasattr(self, 'canvas_label'):
-    #         self.canvas_label = NameLabel()
-    #         self.canvas_label.setStyleSheet(
-    #                         'font-weight: bold; font-size: 18px')
-    #     self.canvas_label.setText(name or 'Model Canvas')
-    #     self.canvas_box.addWidget(self.canvas_label)
-    #     self.canvas_label.setAlignment(Qt.AlignLeft|Qt.AlignTop)
-    #     if widget:
-    #         self.canvas_widget = widget
-    #     else:
-    #         self.canvas_widget = PlaceHolder(image=self.logo, parent=self)
-    #     self.canvas_box.addWidget(self.canvas_widget)
-    #     self.canvas_box.setStretch(0, 0)
-    #     self.canvas_box.setStretch(1, 1)
-    #     self.canvas_box.setAlignment(Qt.AlignLeft|Qt.AlignTop)
-
-    # def create_new_model(self, event):
-    #     if isinstance(self.obj, orb.classes['Identifiable']):
-    #         # TODO:  check for parameters; if found, add them
-    #         orb.log.info('* ConOpsModeler: creating new Model for '
-    #                      'Product with id "%s"' % self.obj.id)
-    #         owner = orb.get(state.get('project'))
-    #         new_model = clone('Model', owner=owner, of_thing=self.obj)
-    #         dlg = PgxnObject(new_model, edit_mode=True, parent=self)
-    #         # dialog.show() -> non-modal dialog
-    #         dlg.show()
-
-
-
 
 if __name__ == '__main__':
     import sys
-    from pangalactic.core.test.utils import (create_test_users,
-                                             create_test_project)
-    from pangalactic.core.serializers import deserialize
     orb.start(home='junk_home', debug=True)
-
-
-
-    serialized_test_objects = create_test_users()
-    serialized_test_objects += create_test_project()
-    deserialize(orb, serialized_test_objects)
-    obj = orb.get('test:twanger')
     app = QApplication(sys.argv)
     mw = ConOpsModeler(external=True, preferred_size=(2000, 1000))
     mw.show()
     sys.exit(app.exec_())
+
