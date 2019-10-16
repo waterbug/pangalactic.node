50f365531bbd6b1b8f5e1aecadf91ac2454f52a4
Steve Waterbury <waterbug@pangalactic.us>
2019-10-16 13:40:53 -0400

    Hm, try an actual repaint() call instead.

--- pangalactic/node/pangalaxian.py
+++ pangalactic/node/pangalaxian.py
@@ -2754,7 +2754,7 @@ class Main(QtWidgets.QMainWindow):
             for column in range(self.dashboard.model().columnCount(
                                                     QModelIndex())):
                 self.dashboard.resizeColumnToContents(column)
-            self.dashboard.update()
+            self.dashboard.repaint()
 
     def update_object_in_trees(self, obj):
         """
