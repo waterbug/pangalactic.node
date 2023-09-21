a621e800361e45847e3500ece0157733ed941ace
Stephen Waterbury <waterbug@pangalactic.us>
2023-09-20 21:17:03 -0400

    Bump version to 4.0.dev0.

--- pangalactic/node/pangalaxian.py
+++ pangalactic/node/pangalaxian.py
@@ -148,7 +148,7 @@ class Main(QMainWindow):
 
     # compatible release versions -- used to determine compatibility of the
     # "home" directory
-    compat_versions = [Version('3.2')]
+    compat_versions = [Version('4.0.dev0')]
 
     # signals
     deleted_object = pyqtSignal(str, str)         # args: oid, cname
