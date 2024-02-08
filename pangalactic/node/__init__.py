43766ca42ed60777aab1027922d41cf72cd56c7c
Stephen Waterbury <waterbug@pangalactic.us>
2024-02-08 17:59:52 -0500

    Bump version to 4.0.dev8.

--- pangalactic/node/__init__.py
+++ pangalactic/node/__init__.py
@@ -2,5 +2,5 @@
 """
 The Pan Galactic Engineering Framework (PGEF) node package.
 """
-__version__ = '4.0.dev7'
+__version__ = '4.0.dev8'
 
--- pangalactic/node/pangalaxian.py
+++ pangalactic/node/pangalaxian.py
@@ -156,7 +156,7 @@ class Main(QMainWindow):
 
     # compatible release versions -- used to determine compatibility of the
     # "home" directory
-    compat_versions = [Version('4.0.dev7')]
+    compat_versions = [Version('4.0.dev8')]
 
     # signals
     deleted_object = pyqtSignal(str, str)         # args: oid, cname
