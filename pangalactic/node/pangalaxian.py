88fa2fd489093a579b2709895646a038eb5abd6e
Stephen Waterbury <waterbug@pangalactic.us>
2024-12-21 10:31:47 -0500

    Fix "compat_versions".

--- pangalactic/node/pangalaxian.py
+++ pangalactic/node/pangalaxian.py
@@ -158,7 +158,11 @@ class Main(QMainWindow):
     # compatible release versions -- used to determine compatibility of the
     # "home" directory
     compat_versions = [
-                       Version('4.3.dev0')
+                       Version('4.2.dev0'),
+                       Version('4.2.dev1'),
+                       Version('4.2'),
+                       Version('4.3.dev0'),
+                       Version('4.3.dev1')
                        ]
 
     # signals
