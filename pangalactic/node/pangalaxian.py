ae2d04178f16fbcda9101e0c689736c6d9c14fd9
Stephen Waterbury <waterbug@pangalactic.us>
2025-04-14 22:08:26 -0400

    Bump version to 4.3.dev11.

--- pangalactic/node/pangalaxian.py
+++ pangalactic/node/pangalaxian.py
@@ -200,7 +200,8 @@ class Main(QMainWindow):
                        Version('4.3.dev7'),
                        Version('4.3.dev8'),
                        Version('4.3.dev9'),
-                       Version('4.3.dev10')
+                       Version('4.3.dev10'),
+                       Version('4.3.dev11')
                        ]
 
     # signals
