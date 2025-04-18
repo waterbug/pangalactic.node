c8e303909467f6903501bff7961d4c6b056fea4f
Stephen Waterbury <waterbug@pangalactic.us>
2025-04-18 16:39:58 -0400

    Add setup.py to bump_version.sh -- now needed for version spec.

--- bump_version.sh
+++ bump_version.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-runsed VERSION pangalactic/node/__init__.py conda/win-64/meta.yaml \
+runsed VERSION setup.py pangalactic/node/__init__.py conda/win-64/meta.yaml \
     conda/linux-64/meta.yaml pangalactic/node/pangalaxian.py
 
